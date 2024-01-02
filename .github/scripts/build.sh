389977e19082ca784f9815535ce5cfbb166236c7
João Viegas <joao.nascimento@rabobank.nl>
2024-01-02 09:10:44 +0100

    add git workflow

---
+++ .github/scripts/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+OUTPUT_DIR=$PWD/dist
+mkdir -p ${OUTPUT_DIR}
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $PWD/target/linux_amr64/config-hub -ldflags "-X github.com/rabobank/config-hub/cfg.Version=${VERSION} -X github.com/rabobank/config-hub/cfg.Commit=${COMMIT}" main.go
+tar czf ${OUTPUT_DIR}/config-hub-linux-amd64-${VERSION}.tgz -C $PWD/target/linux_amr64 config-hub
