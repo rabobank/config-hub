8840cab49287c9663e7c6f4292bfe292c2fe5598
João Viegas <joao.nascimento@rabobank.nl>
2024-01-02 09:27:50 +0100

    fix build script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,5 +3,5 @@
 OUTPUT_DIR=$PWD/dist
 mkdir -p ${OUTPUT_DIR}
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $PWD/target/linux_amr64/config-hub -ldflags "-X github.com/rabobank/config-hub/cfg.Version=${VERSION} -X github.com/rabobank/config-hub/cfg.Commit=${COMMIT}" main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $PWD/target/linux_amr64/config-hub -ldflags "-X github.com/rabobank/config-hub/cfg.Version=${VERSION} -X github.com/rabobank/config-hub/cfg.Commit=${COMMIT}"
 tar czf ${OUTPUT_DIR}/config-hub-linux-amd64-${VERSION}.tgz -C $PWD/target/linux_amr64 config-hub
