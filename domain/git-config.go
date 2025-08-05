f687270bcea662501f4e6942febb76420f434db5
João Viegas <joao.nascimento@rabobank.nl>
2025-08-05 16:36:34 +0200

    fix type conflict in sprintf

--- domain/git-config.go
+++ domain/git-config.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/gomatbase/csn"
+	"github.com/rabobank/config-hub/util"
 )
 
 const (
@@ -58,7 +59,7 @@ func stringOrNull(value *string) string {
 
 func (gc *GitConfig) String() string {
 	return fmt.Sprintf("GitConfig{Uri:%s, DeepClone: %v, DefaultLabel:%s, SearchPaths:%s, Username:%s, Password:%v, PrivateKey:%v, SkipSslValidation:%v, FailOnFetch: %v, AzMiId: %s}",
-		gc.Uri, gc.DeepClone, stringOrNull(gc.DefaultLabel), gc.SearchPaths, stringOrNull(gc.Username), gc.Password != nil && len(*gc.Password) != 0, gc.PrivateKey != nil && len(*gc.PrivateKey) != 0, gc.SkipSslValidation, gc.FailOnFetch, gc.AzMiId)
+		gc.Uri, gc.DeepClone, stringOrNull(gc.DefaultLabel), gc.SearchPaths, stringOrNull(gc.Username), gc.Password != nil && len(*gc.Password) != 0, gc.PrivateKey != nil && len(*gc.PrivateKey) != 0, gc.SkipSslValidation, gc.FailOnFetch, util.EmptyIfNil(gc.AzMiId))
 }
 
 func (gc *GitConfig) Type() string {
